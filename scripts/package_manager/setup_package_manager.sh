ad1342a961979ca5dbff6997ce1d9ba65a517d80
KristijanZic <kristijan.zic@gmail.com>
2023-08-08 02:43:08 +0200

    package_manager: exploring Homebrew install on macOS

--- scripts/package_manager/setup_package_manager.sh
+++ scripts/package_manager/setup_package_manager.sh
@@ -1,4 +1,7 @@
+source $HOME/.dotfiles/scripts/package_manager/install_package_manager_macos.sh
+
 setup_package_manager() {
+
     local package_manager="${1}"
 
     if [[ "$OSTYPE" == "linux-gnu"* ]]; then
@@ -13,7 +16,10 @@ setup_package_manager() {
             echo "${Red}[✗]${Style_Off} Package manager not installed"
             echo "    ${Yellow}!${Style_Off} ${Bold}Please install Homebrew${Style_Off}"
             echo "      Follow the installation instructions at https://brew.sh/"
+
+            queryUser "brew" "Do you want to install Homebrew? (y/N)" "${Green}[✓]${Style_Off} ${Bold}Homebrew installed successfully!${Style_Off}" "Homebrew install rejected..." "Invalid input." "${Yellow}!${Style_Off} ${Bold}Timeout reached.${Style_Off}\nContinuing the script..." 8 install_package_manager_macos
         fi
 
     fi
+
 }
